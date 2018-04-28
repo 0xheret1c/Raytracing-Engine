52bac55c3a3fb59abf72ddcb223d385db68fe35e
Marius <9764028+0xheret1c@users.noreply.github.com>
2018-04-28 22:30:23 +0200

    added various classes

---
+++ Raytracing/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "./Vector.h"
+#include "./Scene.h"
+
+class Core 
+{
+private:
+
+public:
+
+};
---
+++ Raytracing/Scene.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdlib.h>
+
+class Scene
+{
+private:
+
+public:
+};
