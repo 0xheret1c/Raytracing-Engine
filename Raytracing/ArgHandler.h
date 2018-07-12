e3c05c542777a3db1010fbe52008e4044034b6e2
Marius <9764028+0xheret1c@users.noreply.github.com>
2018-07-12 23:08:08 +0200

    added help

--- Raytracing/ArgHandler.h
+++ Raytracing/ArgHandler.h
@@ -4,6 +4,19 @@ class ArgHandler
 {
 private:
 	static void loadSceneFromPath(const char* path){}
+	static void setBounces(unsigned int value) {}
+	static void setFov(unsigned int value) {}
+	static void setDensity(unsigned int value){}
+	static void setGlobalIllumination(unsigned int value){}
+	static void printHelp()
+	{
+		std::cout << "-p or --path                 <path>. Loads and renders a scene from the given path." << std::endl;
+		std::cout << "-f or --fov                 <value>. Sets the fov to the given value." << std::endl;
+		std::cout << "-d or --density             <value>. Sets the density to the given value." << std::endl;
+		std::cout << "-b or --bounces             <value>. Sets the bounces to the given value." << std::endl;
+		std::cout << "-g or --global-illumination <value>. Sets the global-illumination to the given value." << std::endl;
+		std::cout << "-h or --help Shows this help screen." << std::endl;
+	}
 public:
 
 	// 0 = Continue after argHandler
@@ -19,14 +32,41 @@ public:
 		}
 		for (int i = 0; i < argc; i++)
 		{
-			if (argv[i] == "-p") // Render scene from path.
+			if (argv[i] == "-p" || argv[i] == "--path") // Render scene from path.
 			{
 				std::cout << "Rendering scene from \"" << argv[++i] << "\"." << std::endl;
 				loadSceneFromPath(argv[i]);
 			}
+			else if(argv[i] == "-b"  || argv[i] == "--bounces")
+			{
+				std::cout << "Set bounces to " << argv[++i] << "."<< std::endl;
+				setBounces(std::stoi(argv[i]));
+			}
+			else if (argv[i] == "-f" || argv[i] == "--fov")
+			{
+				std::cout << "Set fov to " << argv[++i] << "." << std::endl;
+				setFov(std::stoi(argv[i]));
+			}
+			else if (argv[i] == "-d" || argv[i] == "--density") 
+			{
+				std::cout << "Set fov to " << argv[++i] << "." << std::endl;
+				setDensity(std::stoi(argv[i]));
+			}
+			else if (argv[i] == "-g" || argv[i] == "--global-illumination")
+			{
+				std::cout << "Set global-illumination to " << argv[++i] << "." << std::endl;
+				setGlobalIllumination(std::stoi(argv[i]));
+			}
+			else if (argv[i] == "-h" || argv[i] == "--help")
+			{
+				printHelp();
+				return -1;
+			}
 			else
 			{
-				std::cout << "Unknown argument \"" << argv[i] << std::endl;
+				std::cout << "Unknown argument \"" << argv[i];
+				std::cout << "\". Type -h or --help for help." << std::endl;
+				std::cout << "Exiting." << std::endl;
 				return -1;
 			}
 		}	
