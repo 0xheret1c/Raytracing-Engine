31d5b6f1f8eeb5409f0fe1c0a33036253cb5d87e
Jonathan <h.indetzki@gmx.de>
2018-07-16 13:49:18 +0200

    Added Bounding Boxes

--- Raytracing/Ray.h
+++ Raytracing/Ray.h
@@ -12,19 +12,27 @@ public:
 	//EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 	Eigen::Vector3d origin;
 	Eigen::Vector3d direction;
+	Eigen::Vector3d n_inv;
 
 	Ray(Eigen::Vector3d _origin, Eigen::Vector3d _dir, Scene* _scene)
 	{
 		scene = _scene;
 		origin = _origin;
 		direction = _dir;
+		direction = direction.normalized();
+		n_inv = Eigen::Vector3d(1.0 / direction.x(), 1.0 / direction.y(), 1.0 / direction.z());
 	}
+
 	Ray(Eigen::Vector3d _origin, Eigen::Vector3d _dir)
 	{
 		origin = _origin;
 		direction = _dir;
+		direction = direction.normalized();
+		n_inv = Eigen::Vector3d(1.0 / direction.x(), 1.0 / direction.y(), 1.0 / direction.z());
 	}
+
 	Ray()
 	{
+
 	}
 };
